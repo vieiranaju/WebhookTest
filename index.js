a7127242eab33b20b44e12ca6d22599009cc0bbe
Ana Julia Vieira <129344535+vieiranaju@users.noreply.github.com>
2025-06-08 16:07:26 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.post('/events', (req, res) => {
+  console.log('Webhook recebido:')
+  console.log(req.body)
+  res.sendStatus(200)
+})
+
+app.listen(3000, () => {
+  console.log('Servidor escutando na porta 3000')
+})
